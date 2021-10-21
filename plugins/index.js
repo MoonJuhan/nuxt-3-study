2a3166c5879417bad6495140f82b78a16d1f9c21
MoonJuhan <codemike@naver.com>
2021-10-21 10:52:32 +0900

    test: Plugins 테스트

---
+++ plugins/index.js
@@ -0,0 +1,6 @@
+import { defineNuxtPlugin } from '#app'
+
+export default defineNuxtPlugin((nuxtApp) => {
+  // 글로벌 프로퍼티 만들기
+  // nuxtApp.vueApp.config.globalProperties.{{$plugins_name}} = {{plugins}}
+})
